474f527737702fd4826f92eb9489cd66438c11ac
lilKriT <lilKriTwebDev@gmail.com>
2026-08-07 23:25:40 +0200

    Added favicon. Added colors, fonts etc.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      Starting. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+      voluptate eveniet, repellat veritatis quaerat sint explicabo deleniti.
+      Recusandae ipsam molestiae est perspiciatis at voluptates vel veritatis.
+      Cupiditate officia eius earum maxime assumenda et veritatis, soluta, cum
+      dignissimos incidunt unde nesciunt quas voluptatibus harum beatae, veniam
+      neque dolore obcaecati consequuntur dicta.
+    </div>
+  );
 }
