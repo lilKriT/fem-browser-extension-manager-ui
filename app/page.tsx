31ba4222b11222e620cc32c9723f0ec5564d9812
lilKriT <lilKriTwebDev@gmail.com>
2026-08-08 23:53:16 +0200

    Starting semantic tokens and dark mode.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 export default function Home() {
   return (
-    <div className="">
-      Starting. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-      voluptate eveniet, repellat veritatis quaerat sint explicabo deleniti.
-      Recusandae ipsam molestiae est perspiciatis at voluptates vel veritatis.
-      Cupiditate officia eius earum maxime assumenda et veritatis, soluta, cum
-      dignissimos incidunt unde nesciunt quas voluptatibus harum beatae, veniam
-      neque dolore obcaecati consequuntur dicta.
-    </div>
+    <section className="">
+      <div>
+        Starting. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+        voluptate eveniet, repellat veritatis quaerat sint explicabo deleniti.
+        Recusandae ipsam molestiae est perspiciatis at voluptates vel veritatis.
+        Cupiditate officia eius earum maxime assumenda et veritatis, soluta, cum
+        dignissimos incidunt unde nesciunt quas voluptatibus harum beatae,
+        veniam neque dolore obcaecati consequuntur dicta.
+      </div>
+    </section>
   );
 }
