510f1b2c4608adebe6f907241921979b1929f76b
lilKriT <lilKriTwebDev@gmail.com>
2026-09-22 23:58:44 +0200

    Styling.

--- app/_components/FilterButtons.tsx
+++ app/_components/FilterButtons.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+const buttonStyle = `bg-filter-button-bg border border-filter-button-border 
+  px-4 py-1 
+  text-xl font-500
+  rounded-full cursor-pointer
+  shadow-[5px_0px_25px_1px] shadow-black/10`;
+
 const FilterButtons = () => {
   return (
-    <div>
-      <button>All</button>
-      <button>Active</button>
-      <button>Inactive</button>
+    <div className="flex gap-4">
+      <button className={buttonStyle}>All</button>
+      <button className={buttonStyle}>Active</button>
+      <button className={buttonStyle}>Inactive</button>
     </div>
   );
 };
